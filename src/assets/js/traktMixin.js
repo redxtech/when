d7dae0ae756c03f775bf1f68825759ad62e05213
Gabe Dunn <gabe.dunn@shaw.ca>
2020-07-24 16:30:34 -0600

    move trakt mixin to own file

---
+++ src/assets/js/traktMixin.js
@@ -0,0 +1,17 @@
+import TraktAPI from './trakt.js'
+
+export default {
+  install(Vue) {
+    Vue.mixin({
+      data() {
+        return {
+          trakt: new TraktAPI(
+            import.meta.env.VITE_TRAKT_API_KEY,
+            import.meta.env.VITE_ORIGIN,
+            import.meta.env.VITE_SERVER
+          )
+        }
+      }
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import TraktAPI from './assets/js/trakt.js'
 import router from './assets/js/router.js'
 import store from './assets/js/store.js'
+import traktMixin from './assets/js/traktMixin.js'
 import './assets/css/tailwind.css'
 import './assets/css/tt-norms.css'
 
-const TraktMixin = {
-  install(Vue) {
-    Vue.mixin({
-      data() {
-        return {
-          trakt: new TraktAPI(
-            import.meta.env.VITE_TRAKT_API_KEY,
-            import.meta.env.VITE_ORIGIN,
-            import.meta.env.VITE_SERVER
-          )
-        }
-      }
-    })
-  }
-}
-
-createApp(App).use(router).use(store).use(TraktMixin).mount('#app')
+createApp(App).use(router).use(store).use(traktMixin).mount('#app')
